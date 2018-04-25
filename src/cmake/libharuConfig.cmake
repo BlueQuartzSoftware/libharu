9a2ce3d6447e2e4f1d38e4760b163f28bb491313
Michael Jackson <mike.jackson@bluequartz.net>
2018-04-25 12:32:52 -0400

    More install rules fixed

--- src/cmake/libharuConfig.cmake
+++ src/cmake/libharuConfig.cmake
@@ -1,4 +1,4 @@
-include("${CMAKE_CURRENT_LIST_DIR}/libharuLibTargets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/libharuTargets.cmake")
 
 set(libharu_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../../include")
 set(libharu_LIB_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../../lib;${CMAKE_CURRENT_LIST_DIR}/../../../bin")
