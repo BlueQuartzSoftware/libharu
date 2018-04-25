83835d9173fa2c03d0d31cc37249c10ad9d41cf8
Michael Jackson <mike.jackson@bluequartz.net>
2018-04-25 13:21:58 -0400

    Fixing Config file to have proper paths

--- src/cmake/libharuConfig.cmake
+++ src/cmake/libharuConfig.cmake
@@ -1,7 +1,7 @@
 include("${CMAKE_CURRENT_LIST_DIR}/libharuTargets.cmake")
 
-set(libharu_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../../include")
-set(libharu_LIB_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../../lib;${CMAKE_CURRENT_LIST_DIR}/../../../bin")
+set(libharu_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../include")
+set(libharu_LIB_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../lib;${CMAKE_CURRENT_LIST_DIR}/../../bin")
 
 set(libharu_BUILD_SHARED_LIBS "@LIBHPDF_SHARED@")
 
