3cfd13e4fe927766c9ecd5f7a1b62130c832bf27
Jared Duffey <jared.duffey@bluequartz.net>
2019-08-20 11:56:10 -0400

    Changed CMake formatting to be more consistent

--- src/cmake/libharuConfig.cmake
+++ src/cmake/libharuConfig.cmake
@@ -4,4 +4,3 @@ set(libharu_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../include")
 set(libharu_LIB_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../lib;${CMAKE_CURRENT_LIST_DIR}/../../bin")
 
 set(libharu_BUILD_SHARED_LIBS "@LIBHPDF_SHARED@")
-
